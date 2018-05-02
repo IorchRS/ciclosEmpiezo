b1c96a66289d5b6544eff962b3eda747c1660054
unknown <you@example.com>
2018-05-02 12:06:41 -0500

    mi primer for en JS

---
+++ js/primerFor.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+var i;
+var resultado="";
+
+        function mifuncion(){
+            
+                
+                
+for (i = 1; i < 11; i++) 
+resultado += i +'<br>';
+    
+
+    document.getElementById("palabra").innerHTML = resultado;
+                
+                
+                
+                
+                }    
+     
