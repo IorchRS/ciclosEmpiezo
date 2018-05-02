6fba41c61434dafe0554aa073b8434b6571216d7
unknown <you@example.com>
2018-05-02 12:25:01 -0500

    ya con el While

---
+++ js/primerWhile.js
@@ -0,0 +1,19 @@
+var i=2;
+var resultado="";
+
+        function mifuncion(){
+            while (i < 21) {
+            resultado += i + '<br>';
+            i=i+2
+            }
+                
+  
+    
+
+    document.getElementById("palabra").innerHTML = resultado;
+                
+                
+                
+                
+                }    
+     
